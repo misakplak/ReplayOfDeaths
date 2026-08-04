8e973c3a513209ede7fbdd91e933c46d517780c3
misakplak <misakplakk@gmail.com>
2026-08-04 12:21:54 +0200

    mySQL shloud work soon

--- src/main/java/misakplak/deathLogging/database/MySqlReplayStorage.java
+++ src/main/java/misakplak/deathLogging/database/MySqlReplayStorage.java
@@ -6,6 +6,7 @@ import org.bson.Document;
 
 import javax.sql.DataSource;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -26,10 +27,10 @@ public class MySqlReplayStorage implements ReplayStorage {
     public void save(Replay replay) {
         Document doc = codec.toDocument(replay);
 
-        String sql = "INSERT INTO replays (replayId, playerId, killerId,  created_at, data) VALUES (?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO replays (replay_id, player_id, killer_id, created_at, data) VALUES (?, ?, ?, ?, ?)";
 
         try (Connection conn = dataSource.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql)){
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setString(1, replay.getReplayId().toString());
             stmt.setString(2, replay.playerId().toString());
@@ -39,47 +40,83 @@ public class MySqlReplayStorage implements ReplayStorage {
             stmt.executeUpdate();
 
         } catch (SQLException e) {
-            DeathLogging.getInstance().getLogger().warning("failed to save replay " + e.getMessage());
+            DeathLogging.getInstance().getLogger().warning("Failed to save replay: " + e.getMessage());
         }
-
-
-
-
     }
 
     @Override
     public Replay load(UUID replayId) {
-        String sql = "SELECT data FROM replays WHERE replayId = ?";
+        String sql = "SELECT data FROM replays WHERE replay_id = ?";
+
         try (Connection conn = dataSource.getConnection();
-        PreparedStatement stmt = conn.prepareStatement(sql)) {
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setString(1, replayId.toString());
-            try(ResultSet rs = stmt.executeQuery()) {
+            try (ResultSet rs = stmt.executeQuery()) {
                 if (!rs.next()) return null;
                 return codec.fromDocument(replayId, Document.parse(rs.getString("data")));
             }
 
-        }catch (SQLException e){
-            DeathLogging.getInstance().getLogger().warning("failed to load replay " + e.getMessage());
+        } catch (SQLException e) {
+            DeathLogging.getInstance().getLogger().warning("Failed to load replay: " + e.getMessage());
             return null;
         }
     }
 
     @Override
     public Replay loadLatest() {
-        return null;
+        String sql = "SELECT replay_id, data FROM replays ORDER BY created_at DESC LIMIT 1";
+
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+
+            if (!rs.next()) return null;
+
+            UUID replayId = UUID.fromString(rs.getString("replay_id"));
+            return codec.fromDocument(replayId, Document.parse(rs.getString("data")));
+
+        } catch (SQLException e) {
+            DeathLogging.getInstance().getLogger().warning("Failed to load latest replay: " + e.getMessage());
+            return null;
+        }
     }
 
     @Override
     public List<ReplaySummary> list(UUID playerId, boolean asKiller, int page, int pageSize) {
-        return List.of();
+        String column = asKiller ? "killer_id" : "player_id";
+        String sql = "SELECT replay_id, created_at FROM replays WHERE " + column
+                + " = ? ORDER BY created_at DESC LIMIT ? OFFSET ?";
+
+        List<ReplaySummary> results = new ArrayList<>();
+
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, playerId.toString());
+            stmt.setInt(2, pageSize);
+            stmt.setInt(3, page * pageSize);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    results.add(new ReplaySummary(
+                            UUID.fromString(rs.getString("replay_id")),
+                            rs.getLong("created_at")
+                    ));
+                }
+            }
+
+        } catch (SQLException e) {
+            DeathLogging.getInstance().getLogger().warning("Failed to list replays: " + e.getMessage());
+        }
+
+        return results;
     }
 
     private void createTable() {
         try (Connection conn = dataSource.getConnection(); Statement stmt = conn.createStatement()) {
             stmt.execute("""
-                
-                    CREATE TABLE IF NOT EXISTS replays (
+                CREATE TABLE IF NOT EXISTS replays (
                     replay_id CHAR(36) PRIMARY KEY,
                     player_id CHAR(36) NOT NULL,
                     killer_id CHAR(36) NULL,
--- src/main/java/misakplak/deathLogging/database/ReplayCodec.java
+++ src/main/java/misakplak/deathLogging/database/ReplayCodec.java
@@ -1,19 +1,215 @@
 package misakplak.deathLogging.database;
 
+import misakplak.deathLogging.misc.SwingHand;
+import misakplak.deathLogging.recordables.*;
 import misakplak.deathLogging.replay.Replay;
 import org.bson.Document;
+import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.entity.EntityDamageEvent;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class ReplayCodec {
 
     public Document toDocument(Replay replay) {
 
+        List<Document> records = new ArrayList<>();
 
+        long lastTick = replay.records().getLast().tick();
+        long firstTick = lastTick - 300;
+
+        for (Recordable record : replay.records()) {
+
+            long tick = Math.max(0, record.tick() - firstTick);
+
+            if (record instanceof DamageRecord damage) {
+                records.add(new Document()
+                        .append("type", "DAMAGE")
+                        .append("tick", tick)
+                        .append("position", position(damage.position()))
+                        .append("damage", damage.damage())
+                        .append("cause", damage.cause().name()));
+            }
+
+            if (record instanceof BlockPlaceRecord blockPlace) {
+                records.add(new Document()
+                        .append("type", "BLOCK_PLACE")
+                        .append("tick", tick)
+                        .append("position", position(blockPlace.position()))
+                        .append("material", blockPlace.material().name()));
+            }
+
+            if (record instanceof BlockBreakRecord blockBreak) {
+                records.add(new Document()
+                        .append("type", "BLOCK_BREAK")
+                        .append("tick", tick)
+                        .append("position", position(blockBreak.position()))
+                        .append("material", blockBreak.material().name()));
+            }
+
+            if (record instanceof PickupItemRecord pickupItem) {
+                records.add(new Document()
+                        .append("type", "ITEM_PICKUP")
+                        .append("tick", tick)
+                        .append("position", position(pickupItem.position()))
+                        .append("material", pickupItem.material().name()));
+            }
+
+            if (record instanceof DropItemRecord dropItem) {
+                records.add(new Document()
+                        .append("type", "ITEM_DROP")
+                        .append("tick", tick)
+                        .append("position", position(dropItem.position()))
+                        .append("material", dropItem.material().name()));
+            }
+
+            if (record instanceof LocationRecord location) {
+                records.add(new Document()
+                        .append("type", "MOVE")
+                        .append("tick", tick)
+                        .append("position", position(location.position())));
+            }
+
+            if (record instanceof EntitySpawnRecord spawn) {
+                records.add(new Document()
+                        .append("type", "ENTITY_SPAWN")
+                        .append("tick", tick)
+                        .append("uuid", spawn.entityId().toString())
+                        .append("entityType", spawn.entityType().name())
+                        .append("position", position(spawn.position())));
+            }
+
+            if (record instanceof EntityMoveRecord move) {
+                records.add(new Document()
+                        .append("type", "ENTITY_MOVE")
+                        .append("tick", tick)
+                        .append("uuid", move.entityId().toString())
+                        .append("position", position(move.position())));
+            }
+
+            if (record instanceof EntityRemoveRecord remove) {
+                records.add(new Document()
+                        .append("type", "ENTITY_REMOVE")
+                        .append("tick", tick)
+                        .append("uuid", remove.entityId().toString()));
+            }
+
+            if (record instanceof SwingArmRecord swingArmRecord) {
+                records.add(new Document()
+                        .append("type", "SWING")
+                        .append("tick", tick)
+                        .append("swing", swingArmRecord.hand().name()));
+            }
+        }
+
+        return new Document()
+                .append("replayId", replay.replayId().toString())
+                .append("playerId", replay.playerId().toString())
+                .append("deathlocation", position(replay.deathlocation()))
+                .append("killerId", replay.killerId() == null ? null : replay.killerId().toString())
+                .append("createdAt", replay.createdAt())
+                .append("records", records);
     }
 
     public Replay fromDocument(UUID replayId, Document document) {
 
+        UUID playerId = UUID.fromString(document.getString("playerId"));
 
+        String killerString = document.getString("killerId");
+        UUID killerId = killerString == null ? null : UUID.fromString(killerString);
+
+        Position deathPosition = position(document.get("deathlocation", Document.class));
+        long createdAt = document.getLong("createdAt");
+
+        List<Document> recordsDocs = document.getList("records", Document.class);
+        List<Recordable> records = new ArrayList<>();
+
+        for (Document record : recordsDocs) {
+            switch (record.getString("type")) {
+
+                case "SWING" -> records.add(new SwingArmRecord(
+                        record.getLong("tick"),
+                        SwingHand.valueOf(record.getString("swing"))
+                ));
+
+                case "DAMAGE" -> records.add(new DamageRecord(
+                        record.getLong("tick"),
+                        position(record.get("position", Document.class)),
+                        record.getDouble("damage"),
+                        EntityDamageEvent.DamageCause.valueOf(record.getString("cause"))
+                ));
+
+                case "BLOCK_PLACE" -> records.add(new BlockPlaceRecord(
+                        record.getLong("tick"),
+                        position(record.get("position", Document.class)),
+                        Material.getMaterial(record.getString("material"))
+                ));
+
+                case "BLOCK_BREAK" -> records.add(new BlockBreakRecord(
+                        record.getLong("tick"),
+                        position(record.get("position", Document.class)),
+                        Material.getMaterial(record.getString("material"))
+                ));
+
+                case "MOVE" -> records.add(new LocationRecord(
+                        record.getLong("tick"),
+                        position(record.get("position", Document.class))
+                ));
+
+                case "ITEM_DROP" -> records.add(new DropItemRecord(
+                        record.getLong("tick"),
+                        position(record.get("position", Document.class)),
+                        Material.getMaterial(record.getString("material"))
+                ));
+
+                case "ITEM_PICKUP" -> records.add(new PickupItemRecord(
+                        record.getLong("tick"),
+                        position(record.get("position", Document.class)),
+                        Material.getMaterial(record.getString("material"))
+                ));
+
+                case "ENTITY_SPAWN" -> records.add(new EntitySpawnRecord(
+                        record.getLong("tick"),
+                        UUID.fromString(record.getString("uuid")),
+                        EntityType.valueOf(record.getString("entityType")),
+                        position(record.get("position", Document.class))
+                ));
+
+                case "ENTITY_MOVE" -> records.add(new EntityMoveRecord(
+                        record.getLong("tick"),
+                        UUID.fromString(record.getString("uuid")),
+                        position(record.get("position", Document.class))
+                ));
+
+                case "ENTITY_REMOVE" -> records.add(new EntityRemoveRecord(
+                        record.getLong("tick"),
+                        UUID.fromString(record.getString("uuid"))
+                ));
+            }
+        }
+
+        return new Replay(replayId, playerId, deathPosition, killerId, createdAt, records);
+    }
+
+    private Document position(Position p) {
+        return new Document()
+                .append("x", p.x())
+                .append("y", p.y())
+                .append("z", p.z())
+                .append("yaw", p.yaw())
+                .append("pitch", p.pitch());
+    }
+
+    private Position position(Document document) {
+        return new Position(
+                document.getDouble("x"),
+                document.getDouble("y"),
+                document.getDouble("z"),
+                document.getDouble("yaw").floatValue(),
+                document.getDouble("pitch").floatValue()
+        );
     }
 }
